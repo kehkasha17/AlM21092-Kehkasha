9d008bc038cd7e54701fd83370eb24284962454a
Kehkasha <skehkasha17@gmail.com>
2021-07-09 21:49:50 -0700

    First Commit

---
+++ AlmaMatter/src/almaMatter/Basic.java
@@ -0,0 +1,10 @@
+package almaMatter;
+
+public class Basic {
+public static void main(String[] args) {
+	int i=100;
+	int j=200;
+	int sum=i+j;
+	System.out.println("The sum of i and j is "+sum);
+}
+}
